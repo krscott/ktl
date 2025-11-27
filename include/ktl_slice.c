34f329735c5aaac693eacef57e33bb7b8fef6754
Kris Scott <kscott91@gmail.com>
2025-11-27 12:12:00 -0600

    Cleanup

--- include/ktl_slice.c
+++ include/ktl_slice.c
@@ -3,29 +3,24 @@
 #include <stdlib.h>
 
 #define KTL_T KTL_TEMPLATE(ktl_slice, _type)
-#define ktl_slice_method(x) KTL_TEMPLATE(ktl_slice, x)
+#define ktl_slice_m(x) KTL_TEMPLATE(ktl_slice, x)
 
-#if KTL_TEMPLATE(ktl_slice, _ord)
+#if ktl_slice_m(_ord)
 
-static int ktl_slice_method(_qsort_cmp_)( //
+static int ktl_slice_m(_qsort_cmp_)( //
     void const *a,
     void const *b
 )
 {
-    return ktl_slice_method(_cmp)(*(KTL_T const *)a, *(KTL_T const *)b);
+    return ktl_slice_m(_cmp)(*(KTL_T const *)a, *(KTL_T const *)b);
 }
 
-void ktl_slice_method(sort)(struct ktl_slice slice)
+void ktl_slice_m(sort)(struct ktl_slice slice)
 {
-    qsort(
-        slice.ptr,
-        slice.len,
-        sizeof(slice.ptr[0]),
-        ktl_slice_method(_qsort_cmp_)
-    );
+    qsort(slice.ptr, slice.len, sizeof(slice.ptr[0]), ktl_slice_m(_qsort_cmp_));
 }
 
 #endif // ord
 
-#undef ktl_slice_method
+#undef ktl_slice_m
 #undef KTL_T

5d7df99ad8731b8ec5775f1851dc1622677efe82
Kris Scott <kscott91@gmail.com>
2025-11-29 22:48:21 -0600

    Remove app, src

--- default.nix
+++ default.nix
@@ -25,20 +25,8 @@ stdenv.mkDerivation {
   '';
 
   installPhase = ''
-    if [[ "$CC" == *"mingw32"* ]]; then
-      # Workaround broken pkgCross cmake install
-      mkdir -p "$out/bin"
-      cp build/app/*.exe "$out/bin"
-      if compgen -G "build/src/*.a" > /dev/null; then
-        mkdir -p "$out/lib"
-        cp build/src/*.a "$out/lib"
-      fi
-    else
-      cmake --install build --prefix $out
-    fi
-
     mkdir -p "$out/include"
-    cp include/*.h "$out/include"
+    cp -r include "$out"
   '';
 
   checkPhase = ''

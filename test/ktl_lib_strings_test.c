d91cfbb6ec02e7c1865ab346daf19f13ed30d2ae
Kris Scott <kscott91@gmail.com>
2025-12-07 19:15:01 -0800

    Remove non str test

--- test/ktl_lib_strings_test.c
+++ test/ktl_lib_strings_test.c
@@ -9,15 +9,6 @@
 #error "Asserts are disabled in release"
 #endif
 
-static void t_ints_are_ord(void)
-{
-    i8 a = 11;
-    i8 b = -20;
-    assert(i8_cmp(&a, &a) == 0);
-    assert(i8_cmp(&a, &b) > 0);
-    assert(i8_cmp(&b, &a) < 0);
-}
-
 static void t_strs(void)
 {
     str a = str_from_terminated("Hello");
@@ -66,7 +57,6 @@ static void t_trim(void)
 
 int main(void)
 {
-    RUN(t_ints_are_ord);
     RUN(t_strs);
     RUN(t_strbufs);
     RUN(t_trim);
